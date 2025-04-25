333ad7c1476014f8c3ef65cb75f8ce764afe2d94
SexyDoGyun <kimdk0903@naver.com>
2025-04-25 13:21:20 +0900

    refactor: showFoodMenu 내부 로직을 분리하여 단일 책임 원칙 적용

--- src/lv4/Kiosk.java
+++ src/lv4/Kiosk.java
@@ -46,8 +46,10 @@ public class Kiosk {
     }
 
     private void showFoodMenu(int inputNum) {
+        Menu selectedMenu = menus.get(inputNum - 1);
+
         while (true) {
-            showMenu(menus,inputNum);
+            showMenuItems(selectedMenu);
             System.out.print("원하시는 주문 번호를 입력하여 주십시오: ");
             int orderNum = sc.nextInt();
             sc.nextLine();
@@ -56,10 +58,8 @@ public class Kiosk {
                 break;
             }
 
-            if (orderNum >= 1 && orderNum <= menus.get(inputNum-1).getMenuItems().size()) {
-                MenuItem menu = menus.get(inputNum - 1).getMenuItems().get(orderNum - 1);
-                System.out.println(menu.getFoodName() + "를 주문하였습니다. 가격은 " + menu.getPrice() + "원 입니다.");
-                System.out.println("메뉴 설명: " + menu.getDescription());
+            if (orderNum >= 1 && orderNum <= selectedMenu.getMenuItems().size()) {
+                handleOrder(selectedMenu, orderNum);
             }
             else {
                 System.out.println("잘못된 주문번호입니다.");
@@ -68,16 +68,22 @@ public class Kiosk {
         }
     }
 
-    private void showMenu(List<Menu> menus, int inputNum) {
+    private void handleOrder(Menu selectedMenu, int orderNum) {
+        MenuItem menu = selectedMenu.getMenuItems().get(orderNum - 1);
+        System.out.println(menu.getFoodName() + "를 주문하였습니다. 가격은 " + menu.getPrice() + "원 입니다.");
+        System.out.println("메뉴 설명: " + menu.getDescription());
+    }
+
+    private void showMenuItems(Menu menu) {
         System.out.println("=====================================================================");
-        System.out.println("[ " + menus.get(inputNum - 1).getMenuCategory() + " MENU ]");
+        System.out.println("[ " + menu.getMenuCategory() + " MENU ]");
         // 메뉴판을 하드코딩하지 않기 위해 반복문을 사용합니다.
         // menu 리스트에 있는 각각의 MenuItem을 꺼내기 위해서
         // 메뉴는 menuItems 리스트에 저장되어 있으며, 리스트의 각 요소는 MenuItem 객체입니다.
         // 인덱스는 0부터 시작하지만, 메뉴 번호는 1부터 출력해야 하므로 i + 1을 사용합니다.
         // 각 항목은 getter를 통해 이름, 가격, 설명을 가져와 출력합니다.
-        for (int i = 0; i < menus.get(inputNum - 1).getMenuItems().size(); i++) {
-            System.out.println((i + 1) + ". " + menus.get(inputNum - 1).getMenuItems().get(i).getFoodName() + "   | " + menus.get(inputNum-1).getMenuItems().get(i).getPrice() + " | " + menus.get(inputNum-1).getMenuItems().get(i).getDescription());
+        for (int i = 0; i < menu.getMenuItems().size(); i++) {
+            System.out.println((i + 1) + ". " + menu.getMenuItems().get(i).getFoodName() + "   | " + menu.getMenuItems().get(i).getPrice() + " | " + menu.getMenuItems().get(i).getDescription());
         }
         System.out.println("0. 뒤로가기");
         System.out.println("=====================================================================");
