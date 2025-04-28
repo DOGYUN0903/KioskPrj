82671551495ac28c3970fe3b68a0df59f428d447
SexyDoGyun <kimdk0903@naver.com>
2025-04-28 15:25:54 +0900

    feat: Cart 클래스에 장바구니 아이템 추가, 수량 감소 기능 구현

---
+++ src/challengeLv1/domain/cart/Cart.java
@@ -0,0 +1,33 @@
+package challengeLv1.domain.cart;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cart {
+
+    private List<CartItem> cartItems = new ArrayList<>();
+
+    public void addItem(CartItem item) {
+        cartItems.add(item);
+    }
+
+    public List<CartItem> getCartItems() {
+        return cartItems;
+    }
+
+    public void clear() {
+        cartItems.clear();
+    }
+
+    public boolean isEmpty() {
+        return cartItems.isEmpty();
+    }
+
+    public int calculateTotalPrice() {
+        int total = 0;
+        for (CartItem item : cartItems) {
+            total += item.getPrice() * item.getQuantity();
+        }
+        return total;
+    }
+}
