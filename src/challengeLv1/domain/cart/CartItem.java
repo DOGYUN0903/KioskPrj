07e6c5189315039d55b7c051df075fcb71ffc1d9
SexyDoGyun <kimdk0903@naver.com>
2025-04-28 15:25:35 +0900

    feat: CartItem 클래스 생성 및 메뉴 항목 수량 관리 기능 추가

---
+++ src/challengeLv1/domain/cart/CartItem.java
@@ -0,0 +1,35 @@
+package challengeLv1.domain.cart;
+
+import challengeLv1.domain.menu.MenuItem;
+
+public class CartItem {
+    private final MenuItem menuItem;
+    private int quantity;
+
+    public CartItem(MenuItem menuItem, int quantity) {
+        this.menuItem = menuItem;
+        this.quantity = quantity;
+    }
+
+    public String getFoodName() {
+        return menuItem.getFoodName();
+    }
+
+    public int getPrice() {
+        return menuItem.getPrice();
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    // 수량 증가
+    public void addQuantity(int quantity) {
+        this.quantity += quantity;
+    }
+
+    // 장바구니 수량 감소
+    public void decreaseQuantity(int quantity) {
+        this.quantity -= quantity;
+    }
+}
