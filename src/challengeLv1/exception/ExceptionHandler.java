4a803d637ce6a88ae963f4184f148102f6d2674d
SexyDoGyun <kimdk0903@naver.com>
2025-04-28 15:27:13 +0900

    feat: ExceptionHandler 클래스 생성 및 숫자 입력 예외 검증 기능 추가

---
+++ src/challengeLv1/exception/ExceptionHandler.java
@@ -0,0 +1,18 @@
+package challengeLv1.exception;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ExceptionHandler {
+
+    public static int safeReadInt(Scanner sc) {
+        try {
+            int input = sc.nextInt();
+            sc.nextLine();
+            return input;
+        } catch (InputMismatchException e) {
+            sc.nextLine();
+            throw new InputException("숫자만 입력 가능합니다.");
+        }
+    }
+}
