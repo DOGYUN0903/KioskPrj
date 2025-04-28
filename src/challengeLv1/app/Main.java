3c85840501f4a0fce341de9938ab0e70bc1ae6c9
SexyDoGyun <kimdk0903@naver.com>
2025-04-28 15:24:53 +0900

    feat: Main 클래스 생성 및 Kiosk 실행 로직 작성

---
+++ src/challengeLv1/app/Main.java
@@ -0,0 +1,14 @@
+package challengeLv1.app;
+
+
+import challengeLv1.domain.menu.MenuFactory;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Kiosk kiosk = new Kiosk(MenuFactory.createMenus(), new Scanner(System.in));
+        kiosk.start();
+
+    }
+}
