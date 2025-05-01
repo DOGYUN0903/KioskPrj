80a1738261f99716ac4b10ca5d543f127828f231
SexyDoGyun <kimdk0903@naver.com>
2025-05-01 10:46:07 +0900

    docs : 주석 추가

--- src/challengeLv1/app/Main.java
+++ src/challengeLv1/app/Main.java
@@ -8,6 +8,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Kiosk kiosk = new Kiosk(MenuFactory.createMenus(), new Scanner(System.in));
+
+        // 키오스크 작동 시작
         kiosk.start();
 
     }
